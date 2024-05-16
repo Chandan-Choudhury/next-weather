5988b47dd57edfdbff798b1915325dcd11956882
Chandan-Choudhury <chandanchoudhury0809@gmail.com>
2024-05-16 18:25:12 +0530

    hotfix: Minor fix in text

--- components/TodayForecast.jsx
+++ components/TodayForecast.jsx
@@ -42,30 +42,8 @@ const TodayForecast = ({ defaultCity, targetHours }) => {
     <section id="today-forecast">
       <div className="today-hero">
         <div className="row ps-4 pt-4">
-          <p>TODAY'S FORECAST</p>
+          <p>TODAY&apos;S FORECAST</p>
         </div>
-        {/* <div className="row p-4">
-          {todayForecast.map((forecast, index) => (
-            <div className="col-4 col-lg-2 mb-4" key={index}>
-              <p className="text-center text-nowrap">{forecast.time}</p>
-              <div className="d-flex justify-content-center align-items-center">
-                <Image
-                  src={forecast.icon}
-                  alt={forecast.icon.substring(
-                    forecast.icon.lastIndexOf("/") + 1,
-                    forecast.icon.lastIndexOf(".svg")
-                  )}
-                  width={50}
-                  height={50}
-                  className="today-weather-icon"
-                />
-              </div>
-              <h3 className="mb-0 ms-2 text-center">
-                {Math.round(forecast.temp)}°
-              </h3>
-            </div>
-          ))}
-        </div> */}
         <div className="d-flex flex-wrap justify-content-around p-4">
           {todayForecast.map((forecast, index) => (
             <div
